a48090e5238aba94e6c459fa6b91c2d8e110c897
zhaozimu <zhaozimu@jd.com>
2018-01-04 19:57:35 +0800

    新增-首页-轮播图根据屏幕自适应

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -6,7 +6,7 @@
       <!-- home -->
       <v-tab-container-item id="tabContainHome">
 
-        <v-swipe class="container-swipe" :auto="0">
+        <v-swipe class="container-swipe" :auto="0" :style="{height:carouselH + 'px'}">
           <v-swipe-item v-for="item in funcMenus">
             <router-link :to="item.href">
               <img :src="item.carousel"/>
@@ -52,7 +52,7 @@
 
     <!-- Footer-menu -->
     <v-tabbar v-model="tabMenu" fixed>
-      <v-tab-item v-for="item in menus" :id="item.id" @click.native="item.native">
+      <v-tab-item v-for="item in menus" :id="item.id" @click.native="tabContain = item.native">
         <img slot="icon" :src="item.src"/>
         {{item.name}}
       </v-tab-item>
@@ -60,6 +60,7 @@
 
   </div>
 </template>
+
 <script>
   import {
     TabContainer as vTabContainer,
@@ -88,51 +89,54 @@
       return {
         tabContain: 'tabContainHome',
         tabMenu: 'tabMenuHome',
-        funcMenus: [{
-          name: '限时团购',
-          href: '/',
-          icon: require('../../assets/images/home.svg'),
-          carousel: require('../../assets/images/home-swipe1.jpg')
-        }, {
-          name: '预约教练',
-          href: '/',
-          icon: require('../../assets/images/home.svg'),
-          carousel: require('../../assets/images/home-swipe1.jpg')
-        }, {
-          name: '拼车服务',
-          href: '/',
-          icon: require('../../assets/images/home.svg'),
-          carousel: require('../../assets/images/home-swipe1.jpg')
-        }, {
-          name: '拼住宿',
-          href: '/',
-          icon: require('../../assets/images/home.svg'),
-          carousel: require('../../assets/images/home-swipe1.jpg')
-        }, {
-          name: '二手交易',
-          href: '/',
-          icon: require('../../assets/images/home.svg'),
-          carousel: require('../../assets/images/home-swipe1.jpg')
-        }],
-        recommends: [{
-          name: '团购推荐',
-          list: [{
-            name: '我是名字',
-            price: '33.33',
-            img: require('../../assets/images/home-swipe1.jpg'),
-            href: '/'
+        carouselH: '0px',
+        funcMenus: [
+          {
+            name: '限时团购',
+            href: '/',
+            icon: require('../../assets/images/home.svg'),
+            carousel: require('../../assets/images/home-swipe1.jpg')
           }, {
-            name: '我是名字',
-            price: '33.33',
-            img: require('../../assets/images/home-swipe1.jpg'),
-            href: '/'
+            name: '预约教练',
+            href: '/',
+            icon: require('../../assets/images/home.svg'),
+            carousel: require('../../assets/images/home-swipe1.jpg')
           }, {
-            name: '我是名字',
-            price: '33.33',
-            img: require('../../assets/images/home-swipe1.jpg'),
-            href: '/'
-          }]
-        },
+            name: '拼车服务',
+            href: '/',
+            icon: require('../../assets/images/home.svg'),
+            carousel: require('../../assets/images/home-swipe1.jpg')
+          }, {
+            name: '拼住宿',
+            href: '/',
+            icon: require('../../assets/images/home.svg'),
+            carousel: require('../../assets/images/home-swipe1.jpg')
+          }, {
+            name: '二手交易',
+            href: '/',
+            icon: require('../../assets/images/home.svg'),
+            carousel: require('../../assets/images/home-swipe1.jpg')
+          }],
+        recommends: [
+          {
+            name: '团购推荐',
+            list: [{
+              name: '我是名字',
+              price: '33.33',
+              img: require('../../assets/images/home-swipe1.jpg'),
+              href: '/'
+            }, {
+              name: '我是名字',
+              price: '33.33',
+              img: require('../../assets/images/home-swipe1.jpg'),
+              href: '/'
+            }, {
+              name: '我是名字',
+              price: '33.33',
+              img: require('../../assets/images/home-swipe1.jpg'),
+              href: '/'
+            }]
+          },
           {
             name: '教练推荐',
             list: [{
@@ -209,24 +213,29 @@
                 }]
               }]
           }],
-        menus: [{
-          name: '首页',
-          id: 'tabMenuHome',
-          native: "tabContain = 'tabContainHome'",
-          src: require('../../assets/images/home.svg')
-        }, {
-          name: '我的',
-          id: 'tabMenuMine',
-          native: "tabContain = 'tabContainMine'",
-          src: require('../../assets/images/mine.svg')
-        }]
+        menus: [
+          {
+            name: '首页',
+            id: 'tabMenuHome',
+            native: 'tabContainHome',
+            src: require('../../assets/images/home.svg')
+          }, {
+            name: '我的',
+            id: 'tabMenuMine',
+            native: 'tabContainMine',
+            src: require('../../assets/images/mine.svg')
+          }]
       }
     },
+    created() {
+      const W = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      this.carouselH = Math.floor(96.00 / 480.00 * W);
+    }
   }
 
 </script>
-<style lang="less">
 
+<style lang="less">
   html body {
     margin: 0;
     padding: 0;
@@ -247,7 +256,6 @@
   }
 
   .container-swipe {
-    height: 200px; // 根据屏幕自适应
     img {
       width: 100%;
       height: auto;
